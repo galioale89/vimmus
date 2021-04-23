6e97dba386a644dc6baee0f15f3c060cd182b9ed
Alexandre Galiotto <vimmus.integracao@gmail.com>
2021-04-23 16:12:52 -0300

    vimmuscg beta v.1

--- model/Configuracao.js
+++ model/Configuracao.js
@@ -14,6 +14,10 @@ const Config = new Schema({
         type: String,
         require: true,  
     },
+    minatr: {
+        type: Number,
+        require: true
+    },    
     minest: {
         type: Number,
         require: true
