cb92d8fb0b5c83c50127b943b94a98880b1c0cff
Alexandre Galiotto <alegaliotto@gmail.com>
2022-03-17 19:19:13 -0300

    Vimmus 2.2

--- routes/gerenciamento.js
+++ routes/gerenciamento.js
@@ -90,14 +90,9 @@ const transporter = nodemailer.createTransport({ // Configura os parâmetros de 
         rejectUnauthorized: false
     }
 })
-aws.config.update({
-    region: 'us-east-1',
-    secretAccessKey: 'fVcP/qf7BggNuk029PF+lTEJQGmNBE9x6zXQc4MQ',
-    accessKeyId: 'AKIAV7ZMQ66NULT346DG',
-})
 
-// var credentials = new aws.SharedIniFileCredentials({ profile: 'vimmusimg' })
-// aws.config.credentials = credentials
+var credentials = new aws.SharedIniFileCredentials({ profile: 'vimmusimg' })
+aws.config.credentials = credentials
 
 var s3 = new aws.S3()
 
